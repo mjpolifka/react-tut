264a8aaca74d0833a557f62d536b73d44d7e2f0c
Mitch Polifka <mjpolifka@gmail.com>
2022-01-16 11:46:39 -0600

    set up vidly per the 'Components' section

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,14 @@
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movies from './components/movies';
 
-function App() {
-  return (
-    <main className='container'>
-      <h1>Hello World</h1>
-    </main>
-  );
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <Movies />
+    );
+  }
 }
-
+ 
 export default App;
