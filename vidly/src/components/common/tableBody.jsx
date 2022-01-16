65a054029a1daf82dfe9e748cc9acc7888165115
Mitch Polifka <mjpolifka@gmail.com>
2022-01-16 15:17:04 -0600

    extracted TableHeader and TableBody components

---
+++ vidly/src/components/common/tableBody.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+class TableBody extends Component {
+  render() { 
+    const {data, columns} = this.props;
+
+    return (
+      <tbody>
+        {data.map(item => (
+          <tr key={item._id}>
+            {columns.map(column => (
+              <td key={this.createKey(item, column)}>{this.renderCell(item, column)}</td>
+            ))}
+          </tr>
+        ))}
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    );
+  }
+
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+    return _.get(item, column.path)
+  }
+
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key)
+  }
+}
+ 
+export default TableBody;
