d511e6a5998beafed6dfb24b8409594811d54792
Mitch Polifka <mjpolifka@gmail.com>
2022-01-16 17:21:36 -0600

    added links to movie titles, updated movieForm component

--- vidly/src/App.jsx
+++ vidly/src/App.jsx
@@ -6,6 +6,7 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
 
 class App extends Component {
   state = {  } 
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar></NavBar>
         <main className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const MovieForm = () => {
+const MovieForm = ({ match, history }) => {
   return (
-    <h1>Movie Form</h1>
+    <div>
+      <h1>Movie Form {match.params.id}</h1>
+      <button onClick={() => history.push("/movies")} className="btn btn-primary">Save</button>
+    </div>
   );
 }
  
