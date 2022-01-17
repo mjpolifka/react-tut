9240c8d42a4ebf2fbc9266692d9550eef73657e8
Mitch Polifka <mjpolifka@gmail.com>
2022-01-17 11:05:12 -0600

    synchronized the state with the contents of the form

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    state = {
+        account: { username: '', password: '' }
+    }
+    // username = React.createRef();
+
+    // componentDidMount() {
+    //     this.username.current.focus();
+    // }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        // const username = this.username.current.value;
+    }
+
+    handleChange = ({ currentTarget: input }) => {
+        const account = { ...this.state.account };
+        account[input.name] = input.value;
+        this.setState({ account });
+    }
+
     render() {
+        const { account } = this.state;
         return (
             <div>
                 <h1>Login</h1>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input id="username" type="text" className="form-control" />
+                        <input
+                            value={account.username}
+                            onChange={this.handleChange}
+                            ref={this.username}
+                            id="username"
+                            name="username"
+                            type="text"
+                            className="form-control" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input id="password" type="text" className="form-control" />
+                        <input
+                            value={account.password}
+                            onChange={this.handleChange}
+                            id="password"
+                            name="password"
+                            type="text"
+                            className="form-control" />
                     </div>
                     <button className="btn btn-primary">Login</button>
                 </form>
