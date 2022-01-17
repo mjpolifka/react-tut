ec6cca1e22f453c2f8fa9217996ad405afad9e3b
Mitch Polifka <mjpolifka@gmail.com>
2022-01-17 12:39:18 -0600

    implemented validation with Joi

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -9,36 +9,32 @@ class LoginForm extends Component {
     }
 
     schema = {
-        username: Joi.string().required(),
-        password: Joi.string().required()
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().required().label('Password')
     }
 
     validate = () => {
-        const result = Joi.validate(this.state.account, this.schema, { abortEarly: false })
+        const options = { abortEarly: false };
+        const { error } = Joi.validate(this.state.account, this.schema, options)
+
+        if (!error) return null;
+
         const errors = {};
-        const { account } = this.state;
-
-        if (account.username.trim() === '') errors.username = 'Username is required.'
-        if (account.password.trim() === '') errors.password = 'Password is required.'
-
-        return Object.keys(errors).length === 0 ? null : errors;
+        for (let item of error.details) errors[item.path[0]] = item.message;
+        return errors;
     }
 
     validateProperty = ({ name, value }) => {
-        if (name === 'username') {
-            if (value.trim() === '') return 'Username is required.';
-        }
-        if (name === 'password') {
-            if (value.trim() === '') return 'Password is required.';
-        }
-
+        const obj = { [name]: value };
+        const schema = { [name]: this.schema[name] }
+        const { error } = Joi.validate(obj, schema);
+        return error ? error.details[0].message : null
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
 
         const errors = this.validate();
-        console.log(errors);
         this.setState({ errors: errors || {} });
         if (errors) return;
     }
@@ -47,7 +43,7 @@ class LoginForm extends Component {
         const errors = { ...this.state.errors };
 
         const errorMessage = this.validateProperty(input);
-        if (errorMessage) errors[input.name];
+        if (errorMessage) errors[input.name] = input.value;
         else delete errors[input.name];
 
         const account = { ...this.state.account };
