915f34b7fe781fc8a6a122f0ed96710d4d3b1180
Mitch Polifka <mjpolifka@gmail.com>
2022-01-17 13:20:46 -0600

    added RegisterForm

--- vidly/src/App.jsx
+++ vidly/src/App.jsx
@@ -8,6 +8,7 @@ import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 
 class App extends Component {
   state = {}
@@ -17,12 +18,13 @@ class App extends Component {
         <NavBar></NavBar>
         <main className="container">
           <Switch>
-            <Route path="/login" component={LoginForm}></Route>
-            <Route path="/movies/:id" component={MovieForm}></Route>
-            <Route path="/movies" component={Movies}></Route>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/movies" />
             <Redirect to="/not-found" />
           </Switch>
