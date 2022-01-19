5393e2110247d7fae9d38269b1955c7270c9b685
Mitch Polifka <mjpolifka@gmail.com>
2022-01-19 17:48:03 -0600

    implemented saving and deleting movies

--- vidly/src/services/movieService.js
+++ vidly/src/services/movieService.js
@@ -16,4 +16,12 @@ export function getMovie(movieId) {
   return http.get(apiEndpoint + '/' + movieId);
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = {...movie};
+    delete body._id;
+    return http.put(apiEndpoint + '/' + movie._id, body)
+  }
+
+  return http.post(apiEndpoint, movie);
+}
