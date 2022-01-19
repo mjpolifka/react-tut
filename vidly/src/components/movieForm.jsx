f0a71abfdf8d54a32c60046d3c36b2e9581ed8e4
Mitch Polifka <mjpolifka@gmail.com>
2022-01-19 17:34:41 -0600

    connected MovieForm to movieService

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 // import { getGenres } from '../services/fakeGenreService';
 import { getGenres } from '../services/genreService';
-import { getMovie, saveMovie } from '../services/fakeMovieService';
+// import { getMovie, saveMovie } from '../services/fakeMovieService';
+import { getMovie, saveMovie } from '../services/movieService';
 import Form from './common/form';
 import Joi from 'joi-browser';
 
@@ -33,10 +34,13 @@ class MovieForm extends Form {
     const movieId = this.props.match.params.id;
     if (movieId === 'new') return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) })
+    try{
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) })
+    }
+    catch (ex) {
+      if (ex.response && ex.response.status === 404) this.props.history.replace("/not-found")
+    }
   }
 
   mapToViewModel(movie) {
@@ -44,7 +48,8 @@ class MovieForm extends Form {
       _id: movie._id,
       title: movie.title,
       genreId: movie.genre._id,
-
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate
     }
   }
 
